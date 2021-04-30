a69cd8424ca7cb03791dd492bd25ebe0683470e0
Ferdinand Keller <ferdinandklr@gmail.com>
2021-04-30 18:28:37 +0200

    :sparkles: séparation en pages

--- Pages/Commande.xaml.cs
+++ Pages/Commande.xaml.cs
@@ -13,16 +13,21 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace VéloMax
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace VéloMax.Pages
 {
-    public sealed partial class MainPage : Page
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Commande : Page
     {
-        public MainPage()
+        public Commande()
         {
             this.InitializeComponent();
         }
 
-        private void Navview_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
