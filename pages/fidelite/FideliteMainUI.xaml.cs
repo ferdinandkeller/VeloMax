b49a783cbefec524f89b637ac9e4cb08a8e3810b
Ferdinand Keller <ferdinandklr@gmail.com>
2021-05-17 12:01:47 +0200

    :sparkles: ptites modif

--- pages/fidelite/FideliteMainUI.xaml.cs
+++ pages/fidelite/FideliteMainUI.xaml.cs
@@ -39,5 +39,59 @@ namespace VéloMax.pages
                     break;
             }
         }
+
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>{
+            ("commande2", typeof(VéloMax.pages.FidelioUI)),
+            ("commande1", typeof(VéloMax.pages.FideliteUI))
+        };
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(
+            string navItemTag,
+            Windows.UI.Xaml.Media.Animation.NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+            var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+            _page = item.Page;
+
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            var preNavPageType = NavigationContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                NavigationContentFrame.Navigate(_page, null, transitionInfo);
+            }
+
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+                                           NavigationViewBackRequestedEventArgs args)
+        {
+            TryGoBack();
+        }
+
+        private bool TryGoBack()
+        {
+            if (!NavigationContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavViewFidelite.IsPaneOpen &&
+                (NavViewFidelite.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavViewFidelite.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+            NavigationContentFrame.GoBack();
+            return true;
+        }
     }
 }
