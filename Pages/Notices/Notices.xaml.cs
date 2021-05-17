ec2b3f9e660fc4e3d508aa867bd7b6494afd03e2
Ro-HW <77053170+Ro-HW@users.noreply.github.com>
2021-05-17 23:23:33 +0200

    Notices

--- Pages/Notices/Notices.xaml.cs
+++ Pages/Notices/Notices.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,21 +16,59 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using VéloMax.bdd;
+using Windows.UI.Core;
+using Windows.System;
+using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Diagnostics;
+using VéloMax.bdd;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace VéloMax.Pages
 {
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
+    public class Notice
+    {
+        
+        public string modele;
+        public string piece;
+        public string quant;
+        public string dates;
+
+        public Notice(int numM,string nomM, int numP,string descriptionP,int quant, DateTime dateIntroP, DateTime dateDiscP)
+        {
+            this.modele = $"[{numM}] " + nomM;
+            this.piece = $"[{numP}] " + descriptionP;
+            this.quant = quant.ToString();
+            this.dates = dateIntroP.ToShortDateString() + "->" + dateDiscP.ToShortDateString();
+        }
+    }
     public sealed partial class Notices : Page
     {
+        
         public Notices()
         {
             this.InitializeComponent();
+            refModeleCombo.ItemsSource = Modele.ListerString();
+            
+            //noticeModele
+        }
+
+        public ReadOnlyCollection<Notice> noticeModele { get; set; }
+
+
+        public static ReadOnlyCollection<Notice> DonnerNotice(string numM)
+        {
+            string requete = "Select numM, nomM,numP, descriptionP, quant, dateIntroP,dateDiscP from compositionmodele natural join modele natural join piece where numM='" + numM + "';";
+            List<Notice> list = new List<Notice>();
+            ControlleurRequetes.SelectionnePlusieurs(requete, (MySqlDataReader reader) => { list.Add(new Notice(reader.GetInt32("numM"), reader.GetString("nomM"), reader.GetInt32("numP"), reader.GetString("descriptionP"), reader.GetInt32("quant"), reader.GetDateTime(" dateIntroP"), reader.GetDateTime(" dateDiscP"))); });
+            return new ReadOnlyCollection<Notice>(list);
+        }
+
+        public void Type_Model_Change(object sender, SelectionChangedEventArgs e)
+        {
+            string numM = e.AddedItems[0].ToString();
+            noticeModele = DonnerNotice(numM);
         }
 
         /*
